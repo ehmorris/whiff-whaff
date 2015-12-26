4d91ae09f3cf6b313fe466bed53c91bd2a3e36fa
Edwin Morris <edwin@rapgenius.com>
2015-12-25 19:24:22 -0500

    remove iron:router
    replacing iron:router with angular-ui-router should make things easier —
    first step is to remove iron:router and any concept beyond "enter a new
    game"

--- ping-pong-angular.js
+++ ping-pong-angular.js
@@ -5,24 +5,15 @@ var dasherize = function(string) {
 Games = new Mongo.Collection('games');
 
 if (Meteor.isClient) {
-  angular.module('ping-pong', ['angular-meteor']);
+  angular.module('ping', ['angular-meteor']);
 
-  angular.module('ping-pong').controller('GamesCtrl', ['$scope', '$meteor', function($scope, $meteor) {
+  angular.module('ping').controller('GamesCtrl', ['$scope', '$meteor', function($scope, $meteor) {
     $scope.games = $meteor.collection(Games);
-
-    Router.route('/group/:name', function() {
-      var selectedGame = $scope.games.findOne({ name: this.params.name });
-      $scope.currentGame = selectedGame;
-    });
 
     $scope.addGame = function(newGame) {
       newGame.name = dasherize(newGame.name);
 
       $scope.games.push(newGame);
-
-      $scope.currentGame = newGame;
-
-      history.pushState(newGame, '', newGame.name);
     };
   }]);
 }
