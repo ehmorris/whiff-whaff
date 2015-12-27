02d82d91d1e0b461f660c70d1a3377dfe8ef5f61
Edwin Morris <edwin@rapgenius.com>
2015-12-27 14:09:47 -0500

    navigate directly to a newly created game
    right now when we try to lookup that game in the database, it's not
    there yet. probably has something to do with hooking into the Promise
    that $state.go() returns, and waiting until the "transition" is done:
    https://github.com/angular-ui/ui-router/wiki/Quick-Reference#stategoto--toparams--options

--- client/games/new-game/new-game.component.js
+++ client/games/new-game/new-game.component.js
@@ -6,12 +6,13 @@ angular.module('ping').directive('newGame', function() {
   return {
     restrict: 'E',
     templateUrl: 'client/games/new-game/new-game.html',
-    controller: function($scope, $meteor) {
+    controller: function($scope, $state, $meteor) {
       $scope.games = $meteor.collection(Games);
 
       $scope.addGame = function(newGame) {
         newGame.name = dasherize(newGame.name);
         $scope.games.push(newGame);
+        $state.go('game', {gameName: newGame.name});
       };
     }
   };
