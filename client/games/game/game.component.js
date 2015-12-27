3f06cc4170d7243c2753e7e10207ad8f25f6f66d
Edwin Morris <edwin@rapgenius.com>
2015-12-27 13:59:30 -0500

    add links to game list using ui-router
    https://github.com/angular-ui/ui-router/wiki/Quick-Reference#ui-sref

--- client/games/game/game.component.js
+++ client/games/game/game.component.js
@@ -2,7 +2,7 @@ angular.module('ping').directive('game', function() {
   return {
     restrict: 'E',
     templateUrl: 'client/games/game/game.html',
-    controller: function($scope, $meteor, $stateParams) {
+    controller: function($scope, $stateParams) {
       $scope.game = Games.findOne({name: $stateParams.gameName});
     }
   };
