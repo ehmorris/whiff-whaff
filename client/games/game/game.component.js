f79efb9b1d0cb283ec0cba7aade9202696236dbe
Edwin Morris <edwin@rapgenius.com>
2015-12-28 01:11:24 -0500

    WIP bad not-working attempt at angularizing
    Seems like *somehow* these Mongo calls need to be wrapped w/
    $meteor, so our angular vars can be two-way bound to the page and the
    db. This isn't quite right, but the "update" refactor seems to be along
    the correct lines.
    
    > We are using the $meteor service to bind our Tasks collection to our
    > $scope.tasks variable. Now every change that will happen to each of
    > those objects will be synced in real time across our stack.
    —https://www.meteor.com/tutorials/angular/collections

--- client/games/game/game.component.js
+++ client/games/game/game.component.js
@@ -3,31 +3,18 @@ angular.module('whiffWhaff').directive('game', function() {
     restrict: 'E',
     templateUrl: 'client/games/game/game.html',
     controller: function($scope, $stateParams, $meteor) {
-      $scope.game = Games.findOne({name: $stateParams.gameName}) || {name: $stateParams.gameName};
+      debugger;
+      $scope.game = $meteor.collection(Games.findOne({name: $stateParams.gameName}));
 
       $scope.addScore = function(amount, team) {
         if (!$scope.game._id) {
-          $scope.game = Games.findOne({name: $scope.game.name});
+          $scope.game = $meteor.collection(Games.findOne({name: $scope.game.name}));
 
-          Games.update({
-            _id: $scope.game._id
-          }, {
-            $set: {
-              teamOne: {score: 0},
-              teamTwo: {score: 0}
-            }
-          });
+          $scope.game.teamOne = {score: 0};
+          $scope.game.teamTwo = {score: 0};
         }
 
-        var new_score = $scope.game[team].score + amount;
-        var update_object = {};
-        update_object[team] = {score: new_score};
-
-        Games.update({
-          _id: $scope.game._id
-        }, {
-          $set: update_object
-        });
+        $scope.game[team].score += amount;
       };
     }
   };
